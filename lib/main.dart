df6d5f41be593db5bcb2538bd4dede299f2c2f1b
hsniemi <78792539+hsniemi@users.noreply.github.com>
2022-06-29 17:29:57 +0300

    added .env file for api keys

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:flutter_app_summer_project/providers/my_locations.dart';
 import 'package:flutter_app_summer_project/providers/pickable_list_provider.dart';
 import 'package:flutter_app_summer_project/themes/custom_theme.dart';
 import 'package:flutter_app_summer_project/ui/home.dart';
+import 'package:flutter_config/flutter_config.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables();
   //Declaring Providers in the runApp function.
   runApp(MultiProvider(
     providers: [
